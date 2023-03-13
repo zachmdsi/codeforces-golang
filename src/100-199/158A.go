dbeaf19b463c2e295c8de41e54ac2280c60c2e10
zachmdsi <zmcmanus@dsi.us>
2023-03-12 23:39:19 -0500

    Solved 158A

---
+++ src/100-199/158A.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+func P158A(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var n, k, a, b int
+	Fscan(in, &n, &k)
+	for i := 0; i < k; i++ {
+		Fscan(in, &a)
+		if a == 0 {
+			Fprint(out, i)
+			return
+		}
+	}
+	for i := k; i < n; i++ {
+		Fscan(in, &b)
+		if a != b {
+			Fprint(out, i)
+			return
+		}
+	}
+	Fprint(out, n)
+}
+
+func main() {
+	P158A(os.Stdin, os.Stdout)
+}
